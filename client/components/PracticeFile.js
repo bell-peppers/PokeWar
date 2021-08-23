b6ee9dcf5baf8c502453be4ae256c4083b73a5ab
Gustavo Allen <gustavoallen92@gmail.com>
2021-08-23 13:42:49 -0400

    saftey commit completed type effectiveness

--- client/components/PracticeFile.js
+++ client/components/PracticeFile.js
@@ -1,54 +1,63 @@
 import { Button } from '@material-ui/core';
 import React, { useEffect, useState } from 'react';
-import firebase from '../../utils/firebase';
+// import firebase from '../../utils/firebase';
+import { damageClass } from '../../utils/TypeEffectiveness';
 
 export default function temp(props) {
   const [name, setName] = useState('');
   const [Trainers, setTrainers] = useState([]);
+  const [moveData, setMoveData] = useState({});
+  const [moveOne, setMoveOne] = useState('');
+  const [target, setTarget] = useState('');
 
-  //gets trainers from the database and puts them into an array,
-  useEffect(() => {
-    const trainerRef = firebase.database().ref('Trainers');
-    trainerRef.on('value', (snapshot) => {
-      const trainers = snapshot.val();
-      let AllTrainers = [];
-      for (let id in trainers) {
-        AllTrainers.push({ id, ...trainers[id] });
-      }
-      console.log(AllTrainers);
-      setTrainers(AllTrainers);
-    });
-  }, []);
-
-  const HandleChange = (evt) => {
-    setName(evt.target.value);
+  const onSubmit = (evt) => {
+    evt.preventDefault;
+    setMoveData(damageClass(moveOne, target));
   };
 
-  //creates a trainer in the database
-  const createTrainer = () => {
-    const trainerRef = firebase.database().ref('Trainers');
-    const Trainer = {
-      name,
-      numOfPokemon: 0,
-    };
+  // //gets trainers from the database and puts them into an array,
+  // useEffect(() => {
+  //   const trainerRef = firebase.database().ref('Trainers');
+  //   trainerRef.on('value', (snapshot) => {
+  //     const trainers = snapshot.val();
+  //     let AllTrainers = [];
+  //     for (let id in trainers) {
+  //       AllTrainers.push({ id, ...trainers[id] });
+  //     }
+  //     console.log(AllTrainers);
+  //     setTrainers(AllTrainers);
+  //   });
+  // }, []);
 
-    trainerRef.push(Trainer);
-    setName('');
-  };
+  // const HandleChange = (evt) => {
+  //   setName(evt.target.value);
+  // };
 
-  const Delete = (user) => {
-    const trainerRef = firebase.database().ref('Trainers').child(user.id);
-    trainerRef.remove();
-  };
+  // //creates a trainer in the database
+  // const createTrainer = () => {
+  //   const trainerRef = firebase.database().ref('Trainers');
+  //   const Trainer = {
+  //     name,
+  //     numOfPokemon: 0,
+  //   };
 
-  const Increment = (user) => {
-    const trainerRef = firebase.database().ref('Trainers').child(user.id);
-    trainerRef.update({ numOfPokemon: user.numOfPokemon + 1 });
-  };
+  //   trainerRef.push(Trainer);
+  //   setName('');
+  // };
+
+  // const Delete = (user) => {
+  //   const trainerRef = firebase.database().ref('Trainers').child(user.id);
+  //   trainerRef.remove();
+  // };
+
+  // const Increment = (user) => {
+  //   const trainerRef = firebase.database().ref('Trainers').child(user.id);
+  //   trainerRef.update({ numOfPokemon: user.numOfPokemon + 1 });
+  // };
 
   return (
     <div>
-      <h1>THIS BETTER WORK!</h1>
+      {/* <h1>THIS BETTER WORK!</h1>
 
       <input type='text' onChange={HandleChange} value={name} />
       <Button variant='outlined' onClick={createTrainer}>
@@ -77,7 +86,29 @@ export default function temp(props) {
             </Button>
           </div>
         );
-      })}
+      })} */}
+      <form onSubmit={onSubmit} style={{ display: 'inline-block' }}>
+        <input
+          type='text'
+          value={moveOne}
+          onChange={(evt) => {
+            setMoveOne(evt.target.value);
+          }}
+        />
+        <input
+          type='text'
+          value={target}
+          onChange={(evt) => {
+            setTarget(evt.target.value);
+          }}
+        />
+        <Button variant='outlined' type='submit'>
+          Get Stats
+        </Button>
+      </form>
+
+      <div>{moveData.damage}</div>
+      <div>{moveData.class}</div>
     </div>
   );
 }
