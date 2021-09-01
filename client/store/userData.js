dcca9c670e83d72af421bd037725f1b94b8dd8b3
malessi5 <mikealessi5@gmail.com>
2021-09-01 11:43:53 -0400

    fixed missing userdata

--- client/store/userData.js
+++ client/store/userData.js
@@ -1,18 +1,39 @@
-import { FIREDB } from '../../utils/firebase';
+import {FIREDB} from '../../utils/firebase';
 
 const GET_USER_DATA = 'GET_USER_DATA';
-const GET_OTHER_USER_DATA = 'GET_OTHER_USER_DATA'
+const GET_OTHER_USER_DATA = 'GET_OTHER_USER_DATA';
+const TOGGLE_SOUND = 'TOGGLE_SOUND';
+const TOGGLE_MUSIC = 'TOGGLE_MUSIC';
+const START_BATTLE_MUSIC = 'START_BATTLE_MUSIC';
+
+const _startBattleMusic = (music) => {
+  return {
+    type: START_BATTLE_MUSIC,
+    music,
+  };
+};
+
+export const _toggleSound = () => {
+  return {
+    type: TOGGLE_SOUND,
+  };
+};
+
+const _toggleMusic = () => {
+  return {
+    type: TOGGLE_MUSIC,
+  };
+};
 
 const _getUserData = (user) => {
-	return {
-		type: GET_USER_DATA,
-		user,
-		myUID: user.uid,
-	};
+  return {
+    type: GET_USER_DATA,
+    user,
+    myUID: user.uid,
+  };
 };
 
 const _getOtherUserData = (otherUser) => {
-
   return {
     type: GET_OTHER_USER_DATA,
     otherUser,
@@ -26,6 +47,7 @@ export const startBattleMusic = (musicSrc) => (dispatch) => {
 };
 
 export const toggleMusic = (music, musicOn) => (dispatch) => {
+  console.log(music);
   if (music) {
     if (!musicOn) {
       music.play();
@@ -41,6 +63,7 @@ export const getUserData = (uid) => async (dispatch) => {
   try {
     await FIREDB.ref('users/' + uid).once('value', (snap) => {
       const data = snap.val();
+      // console.log(data)
       return dispatch(_getUserData(data));
     });
   } catch (error) {
@@ -52,21 +75,36 @@ export const getOtherUserData = (uid) => async (dispatch) => {
   try {
     await FIREDB.ref('users/' + uid).once('value', (snap) => {
       const data = snap.val();
+      console.log(data);
       return dispatch(_getOtherUserData(data));
     });
   } catch (error) {
     console.error(error);
   }
-
 };
 
-const initialState = { user: {}, myUID: '', otherUser: {} };
+const initialState = {
+  user: {},
+  myUID: '',
+  otherUser: {},
+  soundOn: true,
+  musicOn: true,
+  currentSong: null,
+};
 
 export default function (state = initialState, action) {
-	switch (action.type) {
-		case GET_USER_DATA:
-			return { ...state, user: action.user};
-		default:
-			return state;
-	}
+  switch (action.type) {
+    case GET_USER_DATA:
+      return {...state, user: action.user, myUID: action.user.uid};
+    case GET_OTHER_USER_DATA:
+      return {...state, otherUser: action.otherUser};
+    case TOGGLE_SOUND:
+      return {...state, soundOn: !state.soundOn};
+    case TOGGLE_MUSIC:
+      return {...state, musicOn: !state.musicOn};
+    case START_BATTLE_MUSIC:
+      return {...state, currentSong: action.music};
+    default:
+      return state;
+  }
 }
