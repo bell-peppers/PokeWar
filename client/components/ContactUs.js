18c9684798b92bf2e9c0201d92508ce05a62886e
Taissiya Ugay <taissiya.ugay@gmail.com>
2021-09-06 16:22:45 -0500

    finished contact us page

--- client/components/ContactUs.js
+++ client/components/ContactUs.js
@@ -1,4 +1,5 @@
-import React, { useState, useRef } from 'react';
+import React from 'react';
+import emailjs from 'emailjs-com';
 import { Button, makeStyles } from '@material-ui/core';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
@@ -6,122 +7,94 @@ import FormControl from '@material-ui/core/FormControl';
 import InputLabel from '@material-ui/core/InputLabel';
 import { OutlinedInput } from '@material-ui/core';
 
-const useStyles = makeStyles(() => ({
-	main: {
-		fontFamily: 'Courier New, monospace',
-		display: 'flex',
-		height: '500px',
-		flexDirection: 'column',
-		justifyContent: 'space-around',
-	},
-	form: {
-		display: 'flex',
-		flexDirection: 'column',
-		width: '350px',
-		border: '3px solid grey',
-		padding: '15px',
-		backgroundColor: 'white',
-		justifyContent: 'space-between',
-		borderRadius: '15px',
-	},
-	signIn: {
-		display: 'flex',
-		justifyContent: 'center',
-		alignItems: 'center',
-		border: '1px solid black',
-		cursor: 'pointer',
-		margin: '5px',
-	},
-}));
-
 export default function ContactUs() {
-	const classes = useStyles();
-	const nameRef = useRef();
-	const emailRef = useRef();
-	const messageRef = useRef();
-	const [error, setError] = useState('');
-	const [loading, setLoading] = useState(false);
-	const [values, setValues] = React.useState({
-		showPassword: false,
-	});
-
-	const handleClickShowPassword = () => {
-		setValues({ ...values, showPassword: !values.showPassword });
-	};
-
-	const handleMouseDownPassword = (event) => {
-		event.preventDefault();
-	};
-	function handleSubmit(e) {
+	function sendEmail(e) {
 		e.preventDefault();
 
-		try {
-			setError('');
-			setLoading(true);
-			login(emailRef.current.value, passwordRef.current.value);
-			history.push('/');
-		} catch (error) {
-			console.error(error);
-			setError(error.message);
-		}
-		setLoading(false);
+		emailjs
+			.sendForm('service_yv8eo6e', 'template_rlk6kyv', e.target, 'user_jga7dj2E0iRJ0pqVmhmrg')
+			.then(
+				(result) => {
+					console.log(result.text);
+				},
+				(error) => {
+					console.log(error.text);
+				}
+			);
+			e.target.reset();
 	}
-
 	return (
-		<div>
+		<div style={{ height: '100vh' }}>
 			<Grid>
 				<Typography
 					style={{
 						fontFamily: 'Courier New, monospace',
 						fontSize: '20px',
+						fontWeight: 'bold',
 						display: 'flex',
 						justifyContent: 'center',
 						alignItems: 'center',
+						padding: '40px',
 					}}
 				>
-					How can we help? Send us a message or question and we'll ep you as
-					soon as we can.
+					{' '}
+					How can we help?
+				</Typography>
+				<Typography
+					style={{
+						fontFamily: 'Courier New, monospace',
+						fontSize: '17px',
+						display: 'flex',
+						justifyContent: 'center',
+						alignItems: 'center',
+						padding: '30px',
+					}}
+				>
+					Send us a message or question and we'll help you as soon as we can.
 				</Typography>
 			</Grid>
-			<Grid style={{ display: 'fixed', right: '30px' }}>
-				<Grid
+			<Grid
+				style={{
+					display: 'flex',
+					justifyContent: 'space-around',
+					maxWidth: '100%',
+				}}
+			>
+				<form
 					style={{
 						display: 'flex',
 						flexDirection: 'column',
-						maxWidth: '450px',
+						width: '450px',
 					}}
+					onSubmit={sendEmail}
 				>
-					<FormControl variant='outlined'>
-						<InputLabel htmlFor='name'>Name</InputLabel>
-						<OutlinedInput id='name' inputRef={nameRef} labelWidth={70} />
+					<FormControl variant='outlined' name='name'>
+						<InputLabel htmlFor='name' name='name'>Name</InputLabel>
+						<OutlinedInput id='name' name='name' labelWidth={70} />
 					</FormControl>
-					<FormControl variant='outlined'>
-						<InputLabel htmlFor='email'>E-mail</InputLabel>
-						<OutlinedInput id='email' inputRef={emailRef} labelWidth={70} />
+					<FormControl variant='outlined' name='email'>
+						<InputLabel htmlFor='email' name='email'>E-mail</InputLabel>
+						<OutlinedInput id='email' name='email' labelWidth={70} />
 					</FormControl>
-					<FormControl variant='outlined'>
-						<InputLabel htmlFor='message'>Message</InputLabel>
-						<OutlinedInput
-							id='message'
-							multiline
-							rows={7}
-							inputRef={messageRef}
-							labelWidth={70}
-						/>
+					<FormControl variant='outlined' name='message'>
+						<InputLabel htmlFor='message' name='message'>Message</InputLabel>
+						<OutlinedInput id='message' name='message' multiline rows={7} labelWidth={70} />
 					</FormControl>
 					<Button
 						variant='contained'
-						disabled={loading}
 						type='submit'
 						style={{
 							width: '100px',
 							position: 'relative',
 							alignSelf: 'center',
+							backgroundColor: 'royalblue',
+							color: 'white',
 						}}
 					>
 						Send
 					</Button>
-				</Grid>
+				</form>
+				<img src='/pics/contactPokes.png' style={{ width: '500px' }} />
 			</Grid>
 		</div>
 	);
